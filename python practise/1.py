27f6eb1d5011d1f0bd25e9a9ed2fdcceee807a38
code_killer <txq0917@126.com>
2018-03-28 21:13:52 +0800

    learn py

---
+++ python practise/1.py
@@ -0,0 +1,9 @@
+#注意顶行输入，否则报错
+print("hello world")#输入
+print(5+3)#print后没f
+#printf(5*3) 报错
+5+3#IDLE可执行，文件中可存在报错
+print(2131234123124123124214*12312312441212412413123)#python 类型无位数限制
+print("hello"+" "+"world")#可执行字符串链接功能
+print("hello\n"*5)#可进行字符串重复
+print("second", "example")
