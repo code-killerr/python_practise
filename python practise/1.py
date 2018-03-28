ca578f98a04f315d83f4de64dd711a0787d80119
code_killer <txq0917@126.com>
2018-03-29 00:34:47 +0800

    learn py

--- python practise/1.py
+++ python practise/1.py
@@ -7,3 +7,17 @@ print(2131234123124123124214*12312312441212412413123)#python 类型无位数限�
 print("hello"+" "+"world")#可执行字符串链接功能
 print("hello\n"*5)#可进行字符串重复
 print("second", "example")
+a=input()#input是一个bif(Built-in functions)，需要后期转换类型
+print(a)
+print(type(a))
+if(int(a)!=3):
+    print("can not change type from str to int")
+else:
+    a=int(a)#不转换默认str类型转换如果遇到非int型数据报错  
+    print(a)
+    print(type(a))
+#dir(__builtins__)#python 提供的bif及其他注意下划线是两个,在IDLE输入框中输入
+#help(xxx)#bif及其他的用法介绍
+    
+    
+
