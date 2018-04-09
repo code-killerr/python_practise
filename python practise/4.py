641918de1210d459cc30ee57756e43a0b2f0991d
code_killer <txq0917@126.com>
2018-04-09 23:25:43 +0800

    learn python

--- python practise/4.py
+++ python practise/4.py
@@ -21,4 +21,19 @@ if a==b:
 elif a<b:
     print("a<b")
 c=a if a<b else b#三元操作符表示a<b为真时c=a否则等于b
-assert 3>4#assert表示断言，当assert后条件为假时终止程序，抛出异常
+#assert 3>4#assert表示断言，当assert后条件为假时终止程序，抛出异常，如若为真无影响
+while a<5:#while循环
+    a+=1
+    print (a)
+#for 目标 in 表达式,表达式可为列表，元组等，for循环可以自动遍历其中元素
+d="abcdefg"
+for i in d:
+    print(i)
+#range配合for使用range([start,]stop[,step=1])]方括号参数表示可选
+for i in range(10):
+    print(i)#如果不指定方括号中参数，range中start默认0 step默认1
+for i in range(1,10,2):#代表从1开始步长为2
+    print(i)
+#break代表终止循环，continue代表跳过后面语句进行循环
+
+ 
