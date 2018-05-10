097957d0b4976fca7f62d5a557843b00a9f66ee0
code_killer <txq0917@126.com>
2018-05-11 00:01:01 +0800

    learn python

---
+++ python practise/异常.py
@@ -0,0 +1,17 @@
+#bbs.fish.com/thread-45814-1-1.html  异常列表
+#1.AssertionError 断言assert失败
+#assert 3>4
+#AttributeError 尝试访问未知对象属性，引用未定义及不存在的东西会报错
+#list=[1,2,3,4]
+#list1.aaaa
+#IndexError 访问未知区域
+#print(list[4])
+#KeyError 调用查找字典中不存在的关键字
+#使用get方法可屏蔽异常
+#NameError 调用不存在的变量
+#print(x)
+#SyntaxError 语法错误
+#print 'aaa'
+#TypeError 不同类型间无效操作
+#1+‘1’
+#ZeroDivision 除数为0报错
