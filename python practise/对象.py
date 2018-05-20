a73002ff7a4e2020a42c71aa85e7c20d78226117
code-killer <txq0917@126.com>
2018-05-21 03:21:15 +0800

    learn pythn

--- python practise/对象.py
+++ python practise/对象.py
@@ -70,4 +70,65 @@ b.hello()
 print('\n')
 #此方法相当于执行
 Parent2.hello(b)
-
+class LittleA:
+    def __init__(self,x):
+        self.num = x
+class LittleB:
+    def __init__(self,x):
+        self.num = x
+class BigAB:
+    def __init__(self,x1,x2):#在其它类中可以调用别的类
+        self.A = LittleA(x1)
+        self.B = LittleB(x2)
+    def printf(self):
+        print("bigAB有 %d个小A和%d个小B" % (self.A.num,self.B.num))
+P = BigAB(5,6)
+P.printf()
+#python中先定义类，定义完成后生成类对象，再由类对象创建实例对象，一个类所有的对象对应一个类对象，由实例对象执行各种方法
+#根据python的特性，相同值的变量属于同一ID，所以如果在类中定义变量，注意不是方法中
+#改变该变量的值，将会改变所有对象中该变量的值，除非在方法中或者自行覆写了该变量
+#这时候相当于改变了变量的ID，给变量一个新的内存，所以不会受到影响，在python中，生成实例对象后其中方法和变量都可以自行覆写
+#甚至可以将对象中的方法覆写成变量，比如可以将关键字方法覆写成变量酱紫的，就是这个特性
+class CC:
+    count = 0
+    def Change(self):
+        self.count = 20
+a = CC()
+b = CC()
+c = CC()
+print(a.count,b.count,c.count)
+a.Change()#此处a调用了Change方法覆写了count，所以不受到CC.count改变的影响,相当于做了类对象的拷贝
+CC.count = 100#改变了类对象的值，类对象的值无法通过实例对象改变，实例对象的值改变只是将其相应的变量覆写，扔到另一个内存中
+print(a.count)
+print(b.count)#其余只是算作将count变量换了个名称而已，所以仍受到CC.count的影响，并不算是拷贝
+print(c.count)
+print(CC.count)
+a.Change=33
+print(a.Change)
+#a.Change()#此时覆写了a中的Change方法，这玩意变成了变量，不可再使用方法形式调用，报错
+#注意类中的方法调用方式和人还不一样，xx.AAA()中，定义AA方法时有一个self，而XX.AA()相当于AA(xx)但类中仍然需要有相关方法
+class AA:
+    def __init__(self):
+        self.count = 0
+    def aa(self):
+        self.count = 10
+        print(self.count)
+    def bb(self):
+        self.count +=10
+        print(self.count)
+    def printf():
+        print('what the fuck!!!!')
+#AA.aa()#报错，因为需要实例化对象来提供self参数
+AA().aa()#可以直接自己实例化自己调用相关方法，如果有构造函数还可以实例自己的同时调用自己的构造函数，卧槽啥玩意
+AA().bb()#如果没有构造函数报错，有构造函数会自动调用构造函数，将实例化的自己塞进self里，这个属于类对象，即类的实例化
+#当然，如果构造函数里没有方法里的变量仍会报错，应该可以证明自己可以实例自己，但是并不会储存相关变量，只是单纯的调用方法而已
+print('\n')
+dd = AA()
+dd.aa()
+dd.bb()#可以对比一下，可以用自己实例化自己来强行调用一些没有外来变量的方法
+print('\n')
+AA.printf()#由于该方法不需要self可以自身调用方法，如果加了self并且函数没有外来变量可以替换成使用类自己实例化自己来调用
+print(AA().__dict__)#这里可以显示自己实例化了自己并调用了构造函数，虽然并不存在相关方法，但是删除类后仍可调用相关方法
+#因为类中方法是静态的，哪怕删除了类,仍然保存在内存中可以调用
+print(AA.__dict__)#这里显示了类中的所有属性
+#理解类对象和实例对象还需要多加练习才能搞明白
