9a0d1cac691d0660c4e00142f821a625d0c7c978
code-killer <txq0917@126.com>
2018-05-28 22:15:23 +0800

    learn python

--- python practise/18.魔法方法.py
+++ python practise/18.魔法方法.py
@@ -9,6 +9,15 @@ class B(str):
     def __new__(cls,string):
         string = string.upper()#执行将字符串大写的操作
         return str.__new__(cls,string)#返回str的__new__魔法方法返回的实例
-a = B("hello python")
+a = B("hello python")#由于当前重写构造函数会造成str的构造方法被覆盖，得不到相应的效果，所以重写当前类的new方法来实现效果后调用str的new方法生成对象，应该这样理解吧
 print(a)
+#__del__析构方法，类结束后调用
+class C():
+    def __del__(self):
+        print('调用del方法')
+c1 = C()
+c2=c1
+del c1#此处并没有调用del方法
+print('\n,分割一下')
+del c2#析构方法调用的条件是没有指向它的东西
     
